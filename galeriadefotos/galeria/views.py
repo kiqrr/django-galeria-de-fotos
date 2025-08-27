f0ba732095ef3085c01c4e7030d1cc788651b26a
kiqrr <caiquerr99@gmail.com>
2025-08-27 19:41:18 -0300

    implementação da funcionalidade da barra de busca

--- galeriadefotos/galeria/views.py
+++ galeriadefotos/galeria/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Photo
 
 def index(request):
     photos = Photo.objects.all()
-    return render(request, 'galeria/index.html', {'photos': photos})
+    
+    # Handle search query
+    search_query = request.GET.get('q')
+    if search_query:
+        photos = photos.filter(
+            Q(title__icontains=search_query) |
+            Q(location__icontains=search_query) |
+            Q(description__icontains=search_query)
+        )
+    
+    context = {
+        'photos': photos,
+        'search_query': search_query,
+    }
+    return render(request, 'galeria/index.html', context)
 
 def photo_detail(request, pk):
     photo = get_object_or_404(Photo, pk=pk)
